65ddcd04e9698c69d3ca86229f6d95e66af190e2
Tan Luc <149952439+TanLuc2410@users.noreply.github.com>
2025-01-15 15:01:42 +0700

    Install and Configure Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "prefer-const": ["error", {
+        "destructuring": "any",
+        "ignoreReadBeforeAssign": false
+      }],
+      "object-shorthand": ["error", "always"],
+      "no-array-constructor": "error",
+      "array-callback-return": "error",
+      "quotes": ["error", "single", { "avoidEscape": true }],
+      "prefer-template": "error",
+      "no-eval": "error",
+      "func-style": ["error", "expression"],
+      "no-loop-func": "error",
+      "prefer-rest-params": "error",
+      "default-param-last": ["error"],
+      "function-paren-newline": ["error", "multiline"],
+      "prefer-arrow-callback": "error",
+      "arrow-body-style": ["error", "as-needed"],
+    }
+  },
+  pluginJs.configs.recommended,
+];
