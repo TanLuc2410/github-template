52e40ddc1ded7a7b633dc0bde6f776b33efd8996
Tan Luc <149952439+TanLuc2410@users.noreply.github.com>
2025-01-16 14:22:47 +0700

    Add eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     languageOptions: { globals: globals.browser },
     rules: {
-      "prefer-const": ["error", {
-        "destructuring": "any",
-        "ignoreReadBeforeAssign": false
-      }],
+      "prefer-const": [
+        "error",
+        {
+          destructuring: "any",
+          ignoreReadBeforeAssign: false,
+        },
+      ],
       "object-shorthand": ["error", "always"],
       "no-unused-vars": "error",
       "no-array-constructor": "error",
       "array-callback-return": "error",
-      "quotes": ["error", "single", { "avoidEscape": true }],
+      quotes: ["error", "single", { avoidEscape: true }],
       "prefer-template": "error",
       "no-eval": "error",
       "func-style": ["error", "expression"],
@@ -25,7 +28,8 @@ export default [
       "function-paren-newline": ["error", "multiline"],
       "prefer-arrow-callback": "error",
       "arrow-body-style": ["error", "as-needed"],
-    }
+    },
   },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
