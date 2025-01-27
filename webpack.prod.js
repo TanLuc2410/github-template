ff0e87a21cfb15bd14930967fc33603a2edbfee2
Tan Luc <149952439+TanLuc2410@users.noreply.github.com>
2025-01-27 16:40:08 +0700

    Reconfigure webpack
    Change the source map used in dev phase

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,10 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: "production",
+  devtool: "source-map",
+  // By enabling source-map in production, you get full source maps
+  // Which could potentially be a security concern (exposing your source code)
+  // If concern about Security problems, switch to *cheap-module-source-map* instead
 });
